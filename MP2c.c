e2e2fabe9fcd1b7ebc86c31dd396a85faa79cd28
amrdgzmn <amor.deguzman@icloud.com>
2024-11-25 15:31:07 +0800

    Include C kernel and make calcsdot function in C kernel

--- MP2c.c
+++ MP2c.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// for calling the C kernel
+#include "MP2Ckernel.c"
+
+// TODO: Use extern to include the asm kernel
+
 int main () {
-	printf("MP2 C file commit");
+	/* declare variables:
+		int n for size of vectors
+		float vectorA[] for VectorA
+		float vectorB[] for VectorB
+		float pointer sdot for storing sdot of VectorA and VectorB
+	*/
+	int n;
+	float vectorA[] = {1.0, 2.0, 3.0, 4.0};
+	float vectorB[] = {5.0, 6.0, 7.0, 8.0};
+	float* sdot;
+	
+	
+	// TODO: Call C kernel and print result
+	calcsdot(n, vectorA, vectorB, sdot);
+	printf("&f", sdot);
+	
+	// TODO: Call the assembly kernel and print result
+	printf("Placeholder for calling assembly code\n");
+	printf("ASM Kernel Results\n")
+	
 	return 0;
 }
--- MP2ckernel.c
+++ MP2ckernel.c
@@ -4,7 +4,11 @@
 #include<MP2c.c>
 
 //include the parameters for the two vectors also
-float calcsdot(int n) {
-	float sdot = 1.0;
+float calcsdot(int n, float[] vectorA, float[] vectorB, float* sdot) {
+	int j; //counter
+	for (j = 0; j < n; j++) {
+		*sdot += (vectorA[j] * vectorB[j]);
+	}
+	//TODO: include how to time this
 	return sdot
 }
