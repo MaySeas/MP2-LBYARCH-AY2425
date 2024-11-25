df3f81beeb00ca42cc893d200a9f5baeb83a8813
amrdgzmn <amor.deguzman@icloud.com>
2024-11-25 13:20:51 +0800

    Add initial C and ASM files

---
+++ MP2asm.asm
@@ -0,0 +1,7 @@
+;MP2 assembly code
+section .text
+global main
+main:
+    ;write your code here
+    xor rax, rax
+    ret
---
+++ MP2c.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+	printf("MP2 C file commit");
+	return 0;
+}
