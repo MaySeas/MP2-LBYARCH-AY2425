8946bed72be4ce2e006db31db93d279a9d831436
amrdgzmn <amor.deguzman@icloud.com>
2024-11-25 22:05:53 +0800

    Change return to float value instead of pointer value

--- MP2ckernel.c
+++ MP2ckernel.c
@@ -1,14 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#include<MP2c.c>
-
-//include the parameters for the two vectors also
-float calcsdot(int n, float[] vectorA, float[] vectorB, float* sdot) {
+float calcsdot(int n, float vectorA[], float vectorB[], float* sdot) {
 	int j; //counter
 	for (j = 0; j < n; j++) {
-		*sdot += (vectorA[j] * vectorB[j]);
+		*sdot += vectorA[j] * vectorB[j];
 	}
 	//TODO: include how to time this
-	return sdot
+	return *sdot;
 }
