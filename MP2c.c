3d7f86e1885e82eac93d60ba4a27a516c6e0d4a1
Rebecca De Veyra <riadeveyra@yahoo.com>
2024-11-26 13:44:52 +0800

    Added c timer, fixed inconsistent results bug

--- MP2c.c
+++ MP2c.c
@@ -12,15 +12,21 @@ int main () {
 		float vectorB[] for VectorB
 		float pointer sdot for storing sdot of VectorA and VectorB
 	*/
-	int i, n=4;
-	float vectorA[] = {10.0, 20.0, 3.0, 40.0};
-	float vectorB[] = {50.0, 6.0, 7.0, 8.0};
+	int n = 4;
+	float vectorA[] = {1.0, 2.0, 3.0, 4.0};
+	float vectorB[] = {5.0, 6.0, 70.0, 8.0};
 	float *sdot = (float *)malloc(sizeof(float));
+	double *ctime = (double *)malloc(sizeof(double)),
+		   *asmtime = (double *)malloc(sizeof(double));
+	*sdot = 0.0;
+	*ctime = 0.0;
+	*asmtime = 0.0;
 	
-	calcsdot(n, vectorA, vectorB, sdot);
+	calcsdot(n, vectorA, vectorB, sdot, ctime);
 	printf("C Kernel\n----------------\n");
-	printf("Dot product from C: %f\n\n", *sdot);
-	
+	printf("Dot product from C: %f\n", *sdot);
+	printf("Execution time: %f nanoseconds\n\n", *ctime);
+		
 	asmsdot(n, vectorA, vectorB, sdot);
 	printf("ASM Kernel\n----------------\n");
 	printf("Dot product from ASM: %f\n\n", *sdot);
--- MP2ckernel.c
+++ MP2ckernel.c
@@ -1,13 +1,26 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 
 //include the parameters for the two vectors also
-float calcsdot(int n, float* vectorA, float* vectorB, float* sdot) {
+float calcsdot(int n, float* vectorA, float* vectorB, float* sdot, double* time) {
+	struct timespec start, end;
+    long nanoseconds;
+    double elapsed;
+	
+	clock_gettime(CLOCK_MONOTONIC, &start);
+	
 	int j; //counter
 	for (j = 0; j < n; j++) {
 		*sdot += vectorA[j] * vectorB[j];
 	}
-	//TODO: include how to time this
-	return *sdot;
+	
+	clock_gettime(CLOCK_MONOTONIC, &end);
+
+	nanoseconds = (end.tv_sec - start.tv_sec) * 1000000000L + (end.tv_nsec - start.tv_nsec);
+    elapsed = nanoseconds / 1000000.0;
+    
+    *time = elapsed;
+	
 	return *sdot;
 }
