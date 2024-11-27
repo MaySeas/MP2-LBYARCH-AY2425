91d59d17abaf67c2dcfd4ed3fc81d1ae38f35bdd
amrdgzmn <amor.deguzman@icloud.com>
2024-11-27 11:49:49 +0800

    Add timer variables and looping for C kernel

--- MP2c.c
+++ MP2c.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
 // for calling the C kernel
 #include "MP2ckernel.c"
 
@@ -12,20 +13,47 @@ int main () {
 		float vectorB[] for VectorB
 		float pointer sdot for storing sdot of VectorA and VectorB
 	*/
-	int n = 4;
-	float vectorA[] = {1.0, 2.0, 3.10, 4.25};
-	float vectorB[] = {5.0, 6.0, 70.20, 8.5};
+	int n = 1048576; // for 2^20 size arrays
+//	int n = 166777216; // for 2^24 size arrays
+//	int n = 1073741824; // for 2^30 size arrays
+	float vectorA[] = {};
+	float vectorB[] = {};
+	
 	double *ctime = (double *)malloc(sizeof(double));
 	double *asmtime = (double *)malloc(sizeof(double));
+	
 	float *sdot = (float *)malloc(sizeof(float)); 
 	*sdot = 0.0;
 	*ctime = 0.0;
 	*asmtime = 0.0;
 	
-	calcsdot(n, vectorA, vectorB, sdot, ctime);
+	int i;
+	// timer variables
+	LARGE_INTEGER li;
+	long long int start, end;
+	double PCFreq, elapse, elapse1;
+	QueryPerformanceCounter(&li);
+	PCFreq = (double)(li.QuadPart);
+	
+	// initialize arrays with the number of elements in n
+	for (i = 0; i < n; i++) {
+		vectorA[i] = 3.0;
+		vectorB[i] = 2.0;
+	}
+	
+	for (i = 0; i < 20; i++) {
+		QueryPerformanceCounter(&li);
+		start - li.QuadPart;
+		calcsdot(n, vectorA, vectorB, sdot);
+		QueryPerformanceCounter(&li);
+		end = li.QuadPart;
+		elapse = ((double)(end-start))* 1000.0 / PCFreq;
+		elapse1 = elapse1 + elapse;
+	}
+	
 	printf("C Kernel\n----------------\n");
 	printf("Dot product from C: %f\n", *sdot);
-	printf("Execution time: %f nanoseconds\n\n", *ctime);
+	printf("Average execution time for 20 runs: %f ms\n\n", elapse1/20);
 	
 	*sdot = 0.0;
 		
--- MP2ckernel.c
+++ MP2ckernel.c
@@ -1,26 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<time.h>
 
-//include the parameters for the two vectors also
-float calcsdot(int n, float* vectorA, float* vectorB, float* sdot, double* time) {
-	struct timespec start, end;
-    long nanoseconds;
-    double elapsed;
-	
-	clock_gettime(CLOCK_MONOTONIC, &start);
-	
-	int j; //counter
+float calcsdot(int n, float* vectorA, float* vectorB, float* sdot) {
+	int j; //counter real
 	for (j = 0; j < n; j++) {
 		*sdot += vectorA[j] * vectorB[j];
 	}
-	
-	clock_gettime(CLOCK_MONOTONIC, &end);
-
-	nanoseconds = (end.tv_sec - start.tv_sec) * 1000000000L + (end.tv_nsec - start.tv_nsec);
-    elapsed = nanoseconds / 1000000.0;
-    
-    *time = elapsed;
-	
 	return *sdot;
 }
